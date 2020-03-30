cccc6155db02dd1f1297139088f62acf71e6dc6c
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-03-30 14:47:45 -0400

    config to local database

--- peckn_project/peckn_project/settings.py
+++ peckn_project/peckn_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_extensions',
+    'peckn_appp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'peckn_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'peckn_database',
+        'USER': 'pecknuser',
+        'PASSWORD': 'peckn',
+        'HOST': 'localhost'
     }
 }
 
