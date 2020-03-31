24b10bccad78863e482f608e3dc177c2df662096
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-03-31 13:23:04 -0400

    created serializer

---
+++ peckn_app/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .model import ProductList, CustomerContact, Order
+
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductList
+        fields = ('id', 'sample_img', 'matierial_input', 'material_quantity' )
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ('id', 'customer_name', 'product', 'dimensions', 'color', 'order_date', 'due_date', 'cost', 'deposit')
+
+class CustomerContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerContact
+        fields = ('id', 'customer_name', 'phone_number', 'customer_address', 'order_history')
