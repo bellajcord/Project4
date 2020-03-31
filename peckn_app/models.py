0da7ff265c45b0564c8c7b68e1739d5b80d4167b
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-03-31 12:19:45 -0400

    created models

--- peckn_app/models.py
+++ peckn_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    customer_name: models.Charfield(max_length=255)
+    product: models.CharField(max_length=400)
+    dimensions: models.CharField(max_length=200)
+    color: models.Charfield(max_length=200)
+    order_date:
+    due_date:
+    cost:
+    deposit:
+
+    def __str__(self):
+        return self.customer_name
+
+class CustomerContact(models.Model):
+    customer_name: models.CharField(max_length=200)
+    phone_number:
+    customer_address:
+    order_history:
+
+    def __str__(self):
+        return self.customer_name
+
+class ProductList (self):
+    input:
+
