ca62bb7d6c53f6ddf09cc749ea12dcff153f4fde
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-04-03 15:37:38 -0400

    styled navbar

--- client/src/components/Navbar_component.jsx
+++ client/src/components/Navbar_component.jsx
@@ -1,18 +1,21 @@
 
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './css/navbar_styles.css';
+
 
 export default class NavBar extends Component {
     render() {
         return (
             <div className='header-fix'>
                 <div className='header'>
-                    <img src='' alt='logo'></img>
+                    <img id='logo_image' src='https://i.imgur.com/zNvI7KS.png?1' alt='logo' ></img>
                 </div>
                 <div className='navContainer'>
-                    <Link to='/' className='nav'><div>Home</div></Link>
-                    <Link to='/Products' className='nav'><div>Products</div></Link>
-                    <Link to='/Orders' className='nav'><div>Orders</div></Link>
+                    <Link to='/' className='nav'><div>Home</div></Link> 
+                    
+                    <Link to='/Products' className='nav'><div>Products</div></Link> 
+                    <Link to='/Orders' className='nav'><div>Orders</div></Link> 
                     <Link to='/Contacts' className='nav'><div>Contact Info</div></Link>
                 </div>
             </div>
